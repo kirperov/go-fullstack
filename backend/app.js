f79c3758aa1011fa907057a12df9fb1ee4358f6a
Kirill Petrov <33541988+kirperov@users.noreply.github.com>
2022-03-04 13:07:40 +0100

    add routes for signup and login

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/users");
 
 mongoose.connect('your config mongodb',
   { useNewUrlParser: true,
@@ -21,6 +22,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
-
+app.use('/api/auth', userRoutes);
   
 module.exports = app;
